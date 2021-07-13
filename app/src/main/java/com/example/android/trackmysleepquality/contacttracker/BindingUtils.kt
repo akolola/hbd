a1466c6712dab5a766ef14749dad24bfa51a5cc1
Alexei Kolola <repoacc@protonmail.com>
2021-07-13 22:12:31 +0200

    HBD-4: Added; <xml> |resource| icon;  Default person.

--- app/src/main/java/com/example/android/trackmysleepquality/contacttracker/BindingUtils.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/contacttracker/BindingUtils.kt
@@ -50,7 +50,7 @@ fun ImageView.setAvatarImage(item: ContactPerson?) {
             3 -> R.drawable.ic_sleep_3
             4 -> R.drawable.ic_sleep_4
             5 -> R.drawable.ic_sleep_5
-            else -> R.drawable.ic_sleep_active
+            else -> R.drawable.ic_default_person
         })
     }
 }
