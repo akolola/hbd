24cf9f25aabc2ef2c1cffb187f96792d5603ab39
Alexei Kolola <kolola@protonmail.com>
2022-08-17 15:12:03 +0200

    HBD-16: Added; (c) ContactBackupFragment; Signin => Google.

--- app/src/main/java/com/example/android/happybirthdates/contactbackup/ContactBackupFragment.kt
+++ app/src/main/java/com/example/android/happybirthdates/contactbackup/ContactBackupFragment.kt
@@ -14,9 +14,12 @@ import com.example.android.happybirthdates.contactdetails.ContactDetailsFragment
 import com.example.android.happybirthdates.database.ContactDatabase
 import com.example.android.happybirthdates.databinding.FragmentContactBackupBinding
 import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.fragment_contact_backup.*
 
 
@@ -26,14 +29,13 @@ private val TAG = "ContactBackupFragment"
 
 class ContactBackupFragment : Fragment() {
 
-/*    companion object {
+   companion object {
         private val RC_SIGN_IN = 9001
     }
 
-    private val mApplication = requireNotNull(this.activity).application
     private var mAuth: FirebaseAuth? = null
-    internal lateinit var mGoogleSignInClient: GoogleSignInClient*/
-    ///private lateinit var viewModel: ContactBackupViewModel
+    internal lateinit var mGoogleSignInClient: GoogleSignInClient
+
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
@@ -43,69 +45,87 @@ class ContactBackupFragment : Fragment() {
         //---------- (c) ContactBackupFragment <- |fragment layout| fragment_contact_backup.
         val binding: FragmentContactBackupBinding  = DataBindingUtil.inflate(inflater, R.layout.fragment_contact_backup, container, false)
 
-/*
+
         //---------- Technical (v) application.
         val application = requireNotNull(this.activity).application
 
         //---------- |navigation| navigation's (v) args.
-        val arguments = ContactDetailsFragmentArgs.fromBundle(arguments!!)
+        //--
 
         //---------- (c) ContactTrackerFragment <- |DB| ContactDatabase.
-        val database = ContactDatabase.getInstance(application).contactDatabaseDao
+        //--
 
 
         //--------------------------- Processing ---------------------------------------------------
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestIdToken("465961236651-soqlh8n5fkqt0c2ufrvel5j5178vkchu.apps.googleusercontent.com").requestEmail().build()
+        mGoogleSignInClient = GoogleSignIn.getClient(application!!, gso)
+        mAuth = FirebaseAuth.getInstance()
+
+
         //-------------------- 'GoogleSignIn' <Button>;
-        binding.signInButton.setOnClickListener(this)
-*/
+        binding.signInButton.setOnClickListener{
+            signInToGoogle()
+        }
+
 
         //--------------------------- Finish -------------------------------------------------------
         return binding.root
     }
 
-/*
-    override fun onClick(v: View) {
-        when(v.id){
-            sign_in_button.id -> signInToGoogle()
-        }
-    }
 
     fun signInToGoogle() {
         val signInIntent = mGoogleSignInClient.signInIntent
         startActivityForResult(signInIntent, RC_SIGN_IN)
     }
-*/
 
 
-/*    override fun onStart() {
+    override fun onStart() {
         super.onStart()
 
         val currentUser = mAuth!!.currentUser
 
         if(currentUser!=null){
             Log.d(TAG,"Currently signed in: " + currentUser.email!!)
-            Toast.makeText(mApplication, "Google logged in: "+ currentUser.email!!, Toast.LENGTH_LONG).show()
+            Toast.makeText(requireActivity(), "Google logged in: "+ currentUser.email!!, Toast.LENGTH_LONG).show()
         }
 
-    }*/
+    }
 
-
-/*    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
         if(requestCode == RC_SIGN_IN){
             val task = GoogleSignIn.getSignedInAccountFromIntent(data)
             try {
                 val account = task.getResult(ApiException::class.java)
-                Toast.makeText(mApplication,"Google sign in succeeded",Toast.LENGTH_LONG).show()
-                //firebaseAuthWithGoogle(account!!)
+                Toast.makeText(requireActivity(),"Google sign in succeeded",Toast.LENGTH_LONG).show()
+                firebaseAuthWithGoogle(account!!)
             } catch (e: ApiException){
                 Log.w(TAG,"Google sign in failed", e)
-                Toast.makeText(mApplication,"Google sign in failed $e",Toast.LENGTH_LONG).show()
+                Toast.makeText(requireActivity(),"Google sign in failed $e",Toast.LENGTH_LONG).show()
             }
         }
 
-    }*/
+    }
+
+    private fun firebaseAuthWithGoogle(acct: GoogleSignInAccount) {
+
+        Log.d(TAG,"firebaseAuthWithGoogle: " + acct.id!!)
+
+        val credential = GoogleAuthProvider.getCredential(acct.idToken, null)
+        mAuth!!.signInWithCredential(credential).addOnCompleteListener(requireActivity()){ task ->
+            if(task.isSuccessful){
+                val user = mAuth!!.currentUser
+                Log.d(TAG,"(m) signInWithCredential: success: currentUser: "+user!!.email!!)
+                Toast.makeText(requireActivity(),"Firebase authentication succeeded", Toast.LENGTH_LONG).show()
+            } else{
+                Log.w(TAG,"(m) signInWithCredential: failure")
+                Toast.makeText(requireActivity(),"Firebase authentication failed", Toast.LENGTH_LONG).show()
+            }
+
+        }
+
+    }
 
 }
 
