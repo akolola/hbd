4e3ee3dfa3469da03de2518d07abd8e52b848fab
Alexei Kolola <kolola@protonmail.com>
2022-11-08 16:15:42 +0100

    HBD-16: Added; (c) ContactBackupFragment; (m) uploadFileToGoogleDrive.

--- app/src/main/java/com/example/android/happybirthdates/contactbackup/ContactBackupFragment.kt
+++ app/src/main/java/com/example/android/happybirthdates/contactbackup/ContactBackupFragment.kt
@@ -1,18 +1,33 @@
 package com.example.android.happybirthdates.contactbackup
 
+import android.content.Context
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.lifecycleScope
 import com.example.android.happybirthdates.R
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.File
+import com.google.api.client.googleapis.extensions.android.gms.auth.UserRecoverableAuthIOException
+import com.google.api.client.http.FileContent
+import com.google.api.services.drive.Drive
 
 class ContactBackupFragment : Fragment() {
 
     companion object {
         fun newInstance() = ContactBackupFragment()
+        private val TAG = "BackupActivity"
+        private val RC_SELECT_FILE = 9111
     }
+
+    lateinit var mDrive: Drive
 
     private lateinit var viewModel: ContactBackupViewModel
 
@@ -27,6 +42,40 @@ class ContactBackupFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(ContactBackupViewModel::class.java)
         // TODO: Use the ViewModel
+    }
+
+    private fun uploadFileToGoogleDrive(context: Context, fileName: String) {
+        mDrive.let { googleDriveService ->
+            lifecycleScope.launch {
+                try {
+
+                    val absolutePath = requireContext().getFileStreamPath(fileName).absolutePath
+                    val jpegFile = File(absolutePath)
+                    val gfile = com.google.api.services.drive.model.File()
+                    gfile.name = "examplepic"
+                    val mimetype = "image/jpeg"
+                    val fileContent = FileContent(mimetype, jpegFile)
+                    ///var fileid = ""
+
+                    withContext(Dispatchers.Main) {
+                        withContext(Dispatchers.IO) {
+                            launch {
+                                var mFile = googleDriveService.Files().create(gfile, fileContent).execute()
+                            }
+                        }
+                    }
+
+                } catch (userAuthEx: UserRecoverableAuthIOException) {
+                    startActivity(
+                        userAuthEx.intent
+                    )
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                    Log.d(TAG, "Error by file(s) upload. ", e)
+                    Toast.makeText(context, "Error by file(s) upload. $e", Toast.LENGTH_LONG).show()
+                }
+            }
+        }
     }
 
 }
