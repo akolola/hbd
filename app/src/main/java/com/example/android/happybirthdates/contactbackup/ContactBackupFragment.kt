e5cf2452d5a294b3c79151866d5dc0d0345b8864
Alexei Kolola <kolola@protonmail.com>
2022-11-12 12:55:24 +0100

    HBD-16: Added; (c) ContactBackupFragment; 'buttonSave' <Button>.

--- app/src/main/java/com/example/android/happybirthdates/contactbackup/ContactBackupFragment.kt
+++ app/src/main/java/com/example/android/happybirthdates/contactbackup/ContactBackupFragment.kt
@@ -8,9 +8,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.Toast
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.lifecycleScope
 import com.example.android.happybirthdates.R
+import com.example.android.happybirthdates.databinding.FragmentContactCloudBinding
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
@@ -18,6 +21,7 @@ import java.io.File
 import com.google.api.client.googleapis.extensions.android.gms.auth.UserRecoverableAuthIOException
 import com.google.api.client.http.FileContent
 import com.google.api.services.drive.Drive
+import kotlinx.android.synthetic.main.fragment_contact_backup.view.*
 
 class ContactBackupFragment : Fragment() {
 
@@ -31,39 +35,45 @@ class ContactBackupFragment : Fragment() {
 
     private lateinit var viewModel: ContactBackupViewModel
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_contact_backup, container, false)
-    }
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(ContactBackupViewModel::class.java)
-        // TODO: Use the ViewModel
+        //--------------------------- Declaration --------------------------------------------------
+        //---------- (c) ContactBackupFragment <- |fragment layout| fragment_contact_backup.
+        val binding = inflater.inflate(R.layout.fragment_contact_backup, container, false)
+
+        //--------------------------- Processing ---------------------------------------------------
+        //-------------------- 'buttonSave' <Button>;
+        //---------- Click listener; (c) ContactBackupFragment -[|DB| Contac]-> GoogleDrive.
+        binding.buttonSave.setOnClickListener {
+            uploadFileToGoogleDrive(requireContext(),"special_day_database")
+        }
+
+        return binding.rootView
     }
 
     private fun uploadFileToGoogleDrive(context: Context, fileName: String) {
+
+        Log.w(TAG,"In (m) setOnClickListener")
+        Toast.makeText(context, "In (m) setOnClickListener", Toast.LENGTH_LONG).show()
+        /*
         mDrive.let { googleDriveService ->
             lifecycleScope.launch {
                 try {
-
                     val absolutePath = requireContext().getFileStreamPath(fileName).absolutePath
-                    val jpegFile = File(absolutePath)
-                    val gfile = com.google.api.services.drive.model.File()
-                    gfile.name = "examplepic"
-                    val mimetype = "image/jpeg"
-                    val fileContent = FileContent(mimetype, jpegFile)
-                    ///var fileid = ""
+                   val jpegFile = File(absolutePath)
+                                       val gfile = com.google.api.services.drive.model.File()
+                                       gfile.name = "examplepic"
+                                       val mimetype = "image/jpeg"
+                                       val fileContent = FileContent(mimetype, jpegFile)
+                                       ///var fileid = ""
 
-                    withContext(Dispatchers.Main) {
-                        withContext(Dispatchers.IO) {
-                            launch {
-                                var mFile = googleDriveService.Files().create(gfile, fileContent).execute()
-                            }
-                        }
-                    }
+                                       withContext(Dispatchers.Main) {
+                                           withContext(Dispatchers.IO) {
+                                               launch {
+                                                   var mFile = googleDriveService.Files().create(gfile, fileContent).execute()
+                                               }
+                                           }
+                                       }
 
                 } catch (userAuthEx: UserRecoverableAuthIOException) {
                     startActivity(
@@ -76,6 +86,13 @@ class ContactBackupFragment : Fragment() {
                 }
             }
         }
+        */
     }
+
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    }
+
 
 }
