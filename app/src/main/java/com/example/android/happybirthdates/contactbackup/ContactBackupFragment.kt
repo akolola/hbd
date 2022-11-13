ab4996b5008582295afa92671da6ebead0040bbe
Alexei Kolola <kolola@protonmail.com>
2022-11-13 13:10:59 +0100

    HBD-16: Added; (c) ContactBackupFragment; (m) getDriveService.

--- app/src/main/java/com/example/android/happybirthdates/contactbackup/ContactBackupFragment.kt
+++ app/src/main/java/com/example/android/happybirthdates/contactbackup/ContactBackupFragment.kt
@@ -14,13 +14,18 @@ import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.lifecycleScope
 import com.example.android.happybirthdates.R
 import com.example.android.happybirthdates.databinding.FragmentContactCloudBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.api.client.extensions.android.http.AndroidHttp
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.io.File
 import com.google.api.client.googleapis.extensions.android.gms.auth.UserRecoverableAuthIOException
 import com.google.api.client.http.FileContent
+import com.google.api.client.json.jackson2.JacksonFactory
 import com.google.api.services.drive.Drive
+import com.google.api.services.drive.DriveScopes
 import kotlinx.android.synthetic.main.fragment_contact_backup.view.*
 
 class ContactBackupFragment : Fragment() {
@@ -41,52 +46,65 @@ class ContactBackupFragment : Fragment() {
         //---------- (c) ContactBackupFragment <- |fragment layout| fragment_contact_backup.
         val binding = inflater.inflate(R.layout.fragment_contact_backup, container, false)
 
+
         //--------------------------- Processing ---------------------------------------------------
         //-------------------- 'buttonSave' <Button>;
-        //---------- Click listener; (c) ContactBackupFragment -[|DB| Contac]-> GoogleDrive.
+        //---------- Click listener; (c) ContactBackupFragment -[|DB| Contact]-> GoogleDrive.
         binding.buttonSave.setOnClickListener {
+            //----------
+            mDrive = getDriveService(this.requireContext())
             uploadFileToGoogleDrive(requireContext(),"special_day_database")
         }
 
         return binding.rootView
     }
 
+    private fun getDriveService(context: Context): Drive {
+        GoogleSignIn.getLastSignedInAccount(context).let {
+            googleAccount ->   val credential = GoogleAccountCredential.usingOAuth2(this.context, listOf(DriveScopes.DRIVE_FILE))
+            credential.selectedAccount = googleAccount!!.account!!
+            return Drive.Builder(AndroidHttp.newCompatibleTransport(), JacksonFactory.getDefaultInstance(), credential).setApplicationName(getString(R.string.app_name)).build()
+        }
+    }
+
+
     private fun uploadFileToGoogleDrive(context: Context, fileName: String) {
 
         Log.w(TAG,"In (m) setOnClickListener")
         Toast.makeText(context, "In (m) setOnClickListener", Toast.LENGTH_LONG).show()
-        /*
+
         mDrive.let { googleDriveService ->
             lifecycleScope.launch {
-                try {
-                    val absolutePath = requireContext().getFileStreamPath(fileName).absolutePath
+
+               try {
+
+                   val absolutePath = requireContext().getFileStreamPath(fileName).absolutePath
                    val jpegFile = File(absolutePath)
-                                       val gfile = com.google.api.services.drive.model.File()
-                                       gfile.name = "examplepic"
-                                       val mimetype = "image/jpeg"
-                                       val fileContent = FileContent(mimetype, jpegFile)
-                                       ///var fileid = ""
+                   val gfile = com.google.api.services.drive.model.File()
+                   gfile.name = "examplepic"
+                   val mimetype = "image/jpeg"
+                   val fileContent = FileContent(mimetype, jpegFile)
+                   ///var fileid = ""
 
-                                       withContext(Dispatchers.Main) {
-                                           withContext(Dispatchers.IO) {
-                                               launch {
-                                                   var mFile = googleDriveService.Files().create(gfile, fileContent).execute()
-                                               }
-                                           }
-                                       }
+                  withContext(Dispatchers.Main) {
+                      withContext(Dispatchers.IO) {
+                          launch {
+                              var mFile = googleDriveService.Files().create(gfile, fileContent).execute()
+                          }
+                      }
+                  }
 
-                } catch (userAuthEx: UserRecoverableAuthIOException) {
-                    startActivity(
-                        userAuthEx.intent
-                    )
-                } catch (e: Exception) {
-                    e.printStackTrace()
-                    Log.d(TAG, "Error by file(s) upload. ", e)
-                    Toast.makeText(context, "Error by file(s) upload. $e", Toast.LENGTH_LONG).show()
-                }
-            }
-        }
-        */
+               } catch (userAuthEx: UserRecoverableAuthIOException) {
+                   startActivity(userAuthEx.intent)
+               } catch (e: Exception) {
+                   e.printStackTrace()
+                   Log.d(TAG, "Error by file(s) upload. ", e)
+                   Toast.makeText(context, "Error by file(s) upload. $e", Toast.LENGTH_LONG).show()
+               }
+
+           }
+       }
+
     }
 
 
