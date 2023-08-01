3e8c11949cdeb9fbbed96e9b63bac5e7e2a4f4f5
Alexei Kolola <repoacc@protonmail.com>
2023-08-01 23:10:35 +0200

    HBD-18: Added; (c) ContactStatusNotificationBackgroundService; Always running service with push up notifications.

--- app/src/main/java/com/example/android/happybirthdates/contacttracker/ContactStatusNotificationBackgroundService.kt
+++ app/src/main/java/com/example/android/happybirthdates/contacttracker/ContactStatusNotificationBackgroundService.kt
@@ -2,11 +2,13 @@ package com.example.android.happybirthdates.contacttracker
 
 import android.app.*
 import android.content.Intent
-import android.graphics.Color
-import android.os.Build
+import android.app.Service
+import android.os.Handler
 import android.os.IBinder
+
 import android.os.SystemClock
 import android.util.Log
+import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
 
@@ -17,6 +19,12 @@ import com.example.android.happybirthdates.R
 private const val TAG = "ConStatNotfnBkgrndServ"
 
 class ContactStatusNotificationBackgroundService : Service() {
+
+
+    private val restartDelay = 1000L // Delay in milliseconds before the service restarts
+    private val handler = Handler()
+    private lateinit var runnable: Runnable
+
 
     //--------------------------- Notification -----------------------------------------------------
     //-------------------- (c) AlarmManager.
@@ -29,9 +37,34 @@ class ContactStatusNotificationBackgroundService : Service() {
     private var mNotificationManager: NotificationManager? = null
 
 
+
+
+
+
+
+    override fun onCreate() {
+        super.onCreate()
+
+        Log.d(TAG, "(m) onCreate. Service created")
+        Toast.makeText(this, "(m) onCreate. Service created.", Toast.LENGTH_LONG).show()
+
+        // Create a runnable to restart the service
+        runnable = Runnable {
+            val intent = Intent(applicationContext, ContactStatusNotificationBackgroundService::class.java)
+            startService(intent)
+        }
+    }
+
+
+
+
+
+
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
 
         Log.i(TAG, "(m) onStartCommand.  Service started by user. Received start id $startId: $intent") ///<-> Toast.makeText(this, "(m) onStartCommand. Service started by user.", Toast.LENGTH_LONG).show()
+        Toast.makeText(this, "(m) onStartCommand. Service started.", Toast.LENGTH_LONG).show()
+
 
         //---------- Technical (v) mContext of app.( (c) Intent & (c) NotificationManager ) <- (c) Context.
         var mContext = applicationContext
@@ -57,30 +90,38 @@ class ContactStatusNotificationBackgroundService : Service() {
 
     }
 
-    override fun onBind(intent: Intent): IBinder? {
-        return null
-    }
 
-    override fun onCreate() {
-        super.onCreate()
-        Log.i(TAG, "(m) onCreate. Service created")
-    }
 
 
     override fun onDestroy() {
+        super.onDestroy()
 
-        Log.i(TAG, "(m) onDestroy. Service stopped.") ///<->Toast.makeText(this, "(m) onDestroy. Service stopped.", Toast.LENGTH_LONG).show()
+        Log.d(TAG, "(m) onDestroy. Service destroyed.")
+        Toast.makeText(this, "(m) onDestroy. Service destroyed.", Toast.LENGTH_LONG).show()
 
         //-------------------- Alarm.
         //---------- (c) AlarmManager Service. Turn off.
-        //alarmManager?.cancel(notifyPendingIntent)
+        ///alarmManager?.cancel(notifyPendingIntent)
         //--------------------
 
         //-------------------- Notification.
         //---------- (c) NotificationManager Service. Turn off.
-        //mNotificationManager!!.cancelAll()
+        ///mNotificationManager!!.cancelAll()
         //--------------------
+
+        // Restart the service after a delay
+        handler.postDelayed(runnable, restartDelay)
     }
+
+
+
+
+
+    override fun onBind(intent: Intent?): IBinder? {
+        // Return null because this service doesn't support binding
+        return null
+    }
+
 
 
 }
